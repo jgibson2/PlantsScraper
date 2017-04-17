ef19ef7025707852a7d009c5374537099968c470
John Gibson <johngibson@wustl.edu>
2017-04-16 20:35:14 -0500

    Improved scraping

--- src/main/java/scraper/PlantsScraper.java
+++ src/main/java/scraper/PlantsScraper.java
@@ -2,17 +2,13 @@ package scraper;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 import au.com.bytecode.opencsv.CSVReader;
-import au.com.bytecode.opencsv.CSVWriter;
 
 import org.apache.commons.io.*;
 
@@ -27,7 +23,8 @@ public class PlantsScraper {
 			String plantID = symbol[0].replaceAll("\"", ""); //plant unique ID
 			String pageString = "https://plants.usda.gov/core/profile?symbol=" + plantID; //URL string for main info page
 			String referenceString = "https://plants.usda.gov/java/reference?symbol=" + plantID; //URL string for references page
-			String charString = "https://plants.usda.gov/java/charProfile?symbol=" + plantID; //URL string for characteristics page
+			//String charString = "https://plants.usda.gov/java/charProfile?symbol=" + plantID + "&format=print"; //URL string for characteristics page
+			String charString = "https://plants.usda.gov/java/charProfile?symbol=" + plantID;
 			
 			/*create URL objects*/
 			
@@ -52,24 +49,24 @@ public class PlantsScraper {
 				System.err.println("Error getting data for symbol " + symbol[0]);
 			}
 			
-			new File(plantID).mkdir(); //create new directory for files to go
+			new File("Plants\\"+plantID).mkdirs(); //create new directory for files to go
 			
 			/*download HTML files into folder*/
 			
 			try {
-				FileUtils.copyURLToFile(pageURL, new File(String.format("%s\\%s.page.html", plantID, plantID)), 1000, 1000);
+				FileUtils.copyURLToFile(pageURL, new File(String.format("Plants\\%s\\%s.page.html", plantID, plantID)), 1000, 1000);
 			} catch (IOException e) {
 				System.err.println("Error getting data for symbol " + symbol[0]);
 			}
 			
 			try {
-				FileUtils.copyURLToFile(referenceURL, new File(String.format("%s\\%s.reference.html", plantID, plantID)), 1000, 1000);
+				FileUtils.copyURLToFile(referenceURL, new File(String.format("Plants\\%s\\%s.reference.html", plantID, plantID)), 1000, 1000);
 			} catch (IOException e) {
 				System.err.println("Error getting data for symbol " + symbol[0]);
 			}
 			
 			try {
-				FileUtils.copyURLToFile(charURL, new File(String.format("%s\\%s.char.html", plantID, plantID)), 1000, 1000);
+				FileUtils.copyURLToFile(charURL, new File(String.format("Plants\\%s\\%s.char.html", plantID, plantID)), 1000, 1000);
 			} catch (IOException e) {
 				System.err.println("Error getting data for symbol " + symbol[0]);
 			}
